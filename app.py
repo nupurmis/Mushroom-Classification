54e919e1ea6e6cfe14dfa33a2674f1f433f4fe1e
Nupur.Mishra <mishranupur2601@gmail.com>
2023-04-12 02:10:29 +0530

    Updated for Flask app

--- app.py
+++ app.py
@@ -4,89 +4,67 @@ Created on Wed Feb 22 23:36:47 2023
 
 @author: Lenovo
 """
-import json 
-import pickle
-from flask import Flask, request, app, jsonify, url_for, render_template
+from flask import Flask,request,app,render_template
 import numpy as np
 import pandas as pd
 
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+
 app = Flask(__name__, template_folder='Templates', static_folder='static')
-#Load the model
-model = pickle.load(open('model.pkl','rb'))
-enc = pickle.load(open('encoding.pkl','rb'))
+
+## Route for a home page
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html') 
 
-@app.route('/predict_api', methods=['POST'])
+@app.route('/predict_data',methods=['POST'])
+def predict_data():
+    print('Get started')
+    data=CustomData(
+        cap_shape=request.form.get('cap-shape'),
+        cap_surface=request.form.get('cap-surface'),
+        cap_color=request.form.get('cap-color'),
+        bruises=request.form.get('bruises'),
+        odor=request.form.get('odor'),
+        gill_attachment=request.form.get('gill-attachment'),
+        gill_spacing=request.form.get('gill-spacing'),
+        gill_size=request.form.get('gill-size'),
+        gill_color=request.form.get('gill-color'),
+        stalk_shape=request.form.get('stalk-shape'),
+        stalk_root=request.form.get('stalk-root'),
+        stalk_surface_above_ring=request.form.get('stalk-surface-above-ring'),
+        stalk_surface_below_ring=request.form.get('stalk-surface-below-ring'),
+        stalk_color_above_ring=request.form.get('stalk-color-above-ring'),
+        stalk_color_below_ring=request.form.get('stalk-color-below-ring'),
+        veil_type=request.form.get('veil-type'),
+        veil_color=request.form.get('veil-color'),
+        ring_number=request.form.get('ring-number'),
+        ring_type=request.form.get('ring-type'),
+        spore_print_color=request.form.get('spore-print-color'),
+        population=request.form.get('population'),
+        habitat=request.form.get('habitat')
+    )
+    print('Data created')
+    pred_df=data.get_data_as_data_frame()
+    print('Data converted as data frame')
+    print(pred_df)
+    print("Before Prediction")
 
-def predict_api():
-    data = request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-#    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output = model.predict(data)
-    print(output[0])
-    return jsonify(output[0])
+    predict_pipeline=PredictPipeline()
+    print("Mid Prediction")
+    results=predict_pipeline.predict(pred_df)
+    print("after Prediction")
+    print(results)
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = [x for x in request.form.values()]
-    input = np.array(data).reshape(1,-1)
-    print(input)
-
-    input = np.delete(input,15)
-    print(input)  
-
-    
-    if input[16] =='n':
-        input[16] = 0
-    elif input[16] == 'o':
-        input[16] = 1
-    else:
-        input[16] = 2
-
-    print(input)  
-
-    if ((input[4] =='a')|(input[4] =='l')):
-        input[4] = 1
-    elif input[4] == 'n':
-        input[4] = 2
-    else:
-        input[4] = 0
-
-    print(input)  
-
-    odor = input[4]
-    ring = input[16]
-    input = np.delete(input,4)    
-    input = np.delete(input,15)
-
-    print(input)    
-
-    input=np.array(input).reshape(1,-1)
-    final_input=enc.transform(np.array(input).reshape(1,-1)).toarray()
-
-    print(final_input)
-
-    final_input=np.append(final_input,[odor])
-    final_input=np.append(final_input,[ring])
-    final_input=final_input.reshape(1,-1)
-    print(final_input)
-     
-    final_input = [float(x) for x in final_input[0]]
-    output = model.predict([final_input])
-    print(output)
-    
     result="Error"
-    if output=='e':
+    if results=='e':
         result="This mushroom is edible"
     else:
         result="This mushroom is poisonous"
 
-    return render_template("home.html", prediction_text=result)
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0',port = 80)
+    return render_template('home.html',prediction_text=result)
     
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port = 80,debug=True)
+    #app.run(debug=True)
